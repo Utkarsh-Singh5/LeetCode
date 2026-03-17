74f75752318ec490a4078f7be97744bfb81298d7
Utkarsh Singh <utkarshsingh52948@gmail.com>
2026-03-17 08:21:59 +0530

    Time: 4 ms (36.39%) | Memory: 46.7 MB (63.35%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -21,30 +21,23 @@ class Solution {
                 {
                     if(s1==ar[i])
                     {
-                        c1++;
+                        c1=1;
                     }
                     if(s3==ar[i])
                     {
-                        c2++;
+                        c2=1;
                     }
                 }
-                if(c1==0 & c2==1)
+                if(c1==0)
                 {
                     l++;
                 }
-                else if(c1==1&c2==0)
+                else if(c2==0)
                 {
                     r--;
-                }
-                else if(c1==0 & c2==0)
-                {
-                    l++;
-                    r--;
-
                 }
                 else
-                {
-                    if(c1+c2==2)
+                
                     {
                         char swap=f[l];
                         f[l]=f[r];
@@ -52,7 +45,7 @@ class Solution {
                         l++;
                         r--;
                     }
-                }
+                
         }return String.valueOf(f);
     }
 }

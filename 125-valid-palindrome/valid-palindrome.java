547f03aac76ae5e02eb9db2376431073fa88850e
Utkarsh Singh <utkarshsingh52948@gmail.com>
2026-03-19 08:40:12 +0530

    Time: 14 ms (32.10%) | Memory: 46.4 MB (31.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,23 +3,16 @@ class Solution {
         
         //if(s==" ")return true;
         String c= s.toLowerCase();
-         
-        int h=1;
         c=c.replaceAll("[^a-z0-9]", "");
        int n= c.length();
 
        for(int i=0;i<c.length()/2;i++)
        {
-        if(c.charAt(i)==c.charAt(n-i-1))
+        if(c.charAt(i)!=c.charAt(n-i-1))
         {
-            h=1;
+            return false;
         }
-        else
-        {
-            h=0;
-            break;
-        }
-       }return h==1;
+       }return true;
         
         
     }
